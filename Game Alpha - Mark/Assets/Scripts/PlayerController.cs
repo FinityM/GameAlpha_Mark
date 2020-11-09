2b5356b58a8dd1158bbe5280da0ef258367d9eab
FinityM <71716915+FinityM@users.noreply.github.com>
2020-11-09 16:07:54 +0000

    Updates Made
    - Moved some game objects to fit aspect ratios
    
    - Made the player move up and down
    
    - Changed the lighting and location
    
    - Worked on the player movement

--- Game Alpha - Mark/Assets/Scripts/PlayerController.cs
+++ Game Alpha - Mark/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private float speed = 20.0f;
     private float verticalInput;
+    private float upperLimit = 16;
+    private float lowerLimit = 2;
     private Rigidbody playerRB;
 
     // Start is called before the first frame update
@@ -17,14 +20,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Get the vertical input 
+        movement();
+        
+    }
+
+    // Method for movement
+    void movement()
+    {
+        // Get the vertical input 
         verticalInput = Input.GetAxis("Vertical");
 
-        //Key presses for up and down
-        
+        // Key presses for up and down the y axis of the position 
+        transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
 
-        
+        // Limit the value the player can move within the y axis for position
+        Mathf.Clamp(transform.position.y, lowerLimit, upperLimit);
 
-        
+
     }
 }
