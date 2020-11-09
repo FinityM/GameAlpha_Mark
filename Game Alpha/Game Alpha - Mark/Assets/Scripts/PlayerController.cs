dff27adec1ae3fbe9dc287c8b8f3caaa0151565a
FinityM <71716915+FinityM@users.noreply.github.com>
2020-11-09 15:06:14 +0000

    Game Alpha
    First publication of Game Alpha for IMM Project

---
+++ Game Alpha/Game Alpha - Mark/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float verticalInput;
+    private Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the vertical input 
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Key presses for up and down
+        
+
+        
+
+        
+    }
+}
